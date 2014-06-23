e0e14fac1166cd7ac443d7429e3cca85aac43181
John Dill <dill.john.d@gmail.com>
2014-06-23 19:34:34 -0400

    added profile and subscription pages with jquery tabs

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -255,6 +255,7 @@ if (document.URL.match(/\/album.html/)) {
 require('./landing');
 require('./collection');
 require('./album');
+require('./profile');
 });
 
 ;require.register("scripts/collection", function(exports, require, module) {
@@ -342,6 +343,28 @@ $(document).ready(function() {
 
   $('.selling-points .point').hover(onHoverAction, offHoverAction);
 });
+});
+
+;require.register("scripts/profile", function(exports, require, module) {
+var tabsContainer = '.user-profile-tabs-container';
+
+var selectTabHandler = function(event) {
+  $tab = $(this);
+  $(tabsContainer + ' li').removeClass('active');
+  $tab.parent().addClass('active');
+  selectedTabName = $tab.attr('href');
+  $('.tab-pane').addClass('hidden');
+  $(selectedTabName).removeClass('hidden');
+  event.preventDefault();
+};
+
+if (document.URL.match(/\/profile.html/)) {
+  $(document).ready(function() {
+    var $tabs = $(tabsContainer + ' a ');
+    $tabs.click(selectTabHandler);
+    $tabs[0].click();
+  });
+}
 });
 
 ;
