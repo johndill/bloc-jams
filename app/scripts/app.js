94049290e5cdd5d9976ecb674064a04ceeec927f
John Dill <dill.john.d@gmail.com>
2014-06-25 20:56:25 -0400

    converted bloc jams to a single page app

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,36 +1,41 @@
-//require('./landing');
-//require('./collection');
-//require('./album');
-//require('./profile');
+angular.module('BlocJams', ['ui.router']).
 
-app = angular.module('BlocJams', []);
+  config(['$stateProvider', '$locationProvider', function($stateProvider, $locationProvider) {
+    $locationProvider.html5Mode(true);
 
-app.controller('Landing.controller', ['$scope', function($scope) {
-  $scope.title = 'Bloc Jams';
-  $scope.subText = 'Turn the music up!';
+    $stateProvider.state('landing', {
+      url: '/',
+      controller: 'Landing.controller',
+      templateUrl: '/templates/landing.html'
+    });
+  }]).
 
-  $scope.subTextClicked = function() {
-    $scope.subText += '!';
-  };
+  controller('Landing.controller', ['$scope', function($scope) {
+    $scope.title = 'Bloc Jams';
+    $scope.subText = 'Turn the music up!';
 
-  $scope.albumURLs = [
-    '/images/album-placeholders/album-1.jpg',
-    '/images/album-placeholders/album-2.jpg',
-    '/images/album-placeholders/album-3.jpg',
-    '/images/album-placeholders/album-4.jpg',
-    '/images/album-placeholders/album-5.jpg',
-    '/images/album-placeholders/album-6.jpg',
-    '/images/album-placeholders/album-7.jpg',
-    '/images/album-placeholders/album-8.jpg',
-    '/images/album-placeholders/album-9.jpg',
-  ];
+    $scope.subTextClicked = function() {
+      $scope.subText += '!';
+    };
 
-  $scope.shuffleAlbums = function() {
-    $scope.albumURLs = shuffle($scope.albumURLs);
-  };
+    $scope.albumURLs = [
+      '/images/album-placeholders/album-1.jpg',
+      '/images/album-placeholders/album-2.jpg',
+      '/images/album-placeholders/album-3.jpg',
+      '/images/album-placeholders/album-4.jpg',
+      '/images/album-placeholders/album-5.jpg',
+      '/images/album-placeholders/album-6.jpg',
+      '/images/album-placeholders/album-7.jpg',
+      '/images/album-placeholders/album-8.jpg',
+      '/images/album-placeholders/album-9.jpg',
+    ];
 
-  function shuffle(o){ //v1.0
-    for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
-    return o;
-  };
-}]);
+    $scope.shuffleAlbums = function() {
+      $scope.albumURLs = shuffle($scope.albumURLs);
+    };
+
+    function shuffle(o){ //v1.0
+      for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+      return o;
+    };
+  }]);
